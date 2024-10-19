cf712c0d49d5a4c083fb834f048d03c1de682722
WhoIsRoi <roicer.romero96@gmail.com>
2024-10-18 22:14:14 -0500

    implementacion del dashboard desde el router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Importa las utilidades de Router
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Auth/Login.jsx';
 import Register from './components/Auth/Register.jsx';
+import Dashboard from './components/Dashboard/Dashboard.jsx'; // Importa el nuevo Dashboard
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
       <Routes>
-        {/* Ruta por defecto que muestra Login */}
         <Route path="/" element={<Login />} />
-        {/* Ruta para el componente Register */}
         <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} /> {/* Nueva ruta para el Dashboard */}
       </Routes>
     </Router>
   </StrictMode>
