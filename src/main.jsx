b12b2365b884610c7f4d31bf2ae0cac4a5dacc93
WhoIsRoi <roicer.romero96@gmail.com>
2024-10-18 22:11:41 -0500

    comit para enrutar los proyectos

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Importa las utilidades de Router
 import Login from './components/Auth/Login.jsx';
 import Register from './components/Auth/Register.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login />
+    <Router>
+      <Routes>
+        {/* Ruta por defecto que muestra Login */}
+        <Route path="/" element={<Login />} />
+        {/* Ruta para el componente Register */}
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   </StrictMode>
 );
